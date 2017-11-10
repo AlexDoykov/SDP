7f091ef3e52444d97b06a4372693cfa3f3afe927
Alexander <doykov70@gmail.com>
2017-11-10 14:09:09 +0200

    Tree methods

---
+++ Tree/BTree.h
@@ -0,0 +1,131 @@
+#include <iostream>
+#include <string>
+template<class T>
+struct Node{
+	T data;
+	Node<T> *left;
+	Node<T> *right;
+	Node(const T& data):data(data), left(nullptr), right(nullptr){
+	}
+};
+
+
+
+template<class T>
+class BTree
+{
+private:
+Node<T>* root;
+	bool insert(const T& data, std::string path, Node<T> *&root){
+		if(root == nullptr && path == ""){
+			root = new Node<T>(data);
+			return true;
+		}
+		
+		if(root == nullptr && path != ""){
+			return false;
+		}
+
+		if(root != nullptr && path == ""){
+			return false;
+		}
+
+		if(path[0] == 'L'){
+			path.erase(path.begin());
+			return insert(data, path, root->left);
+		}
+		if(path[0] == 'R'){
+			path.erase(path.begin());
+			return insert(data, path, root->right);
+		}	
+		return false;
+	}
+
+	unsigned count(Node<T> *root) const{
+		if(root == nullptr){
+			return 0;
+		}
+		return 1 + count(root->left) + count(root->right);
+	}
+
+	unsigned countEvens(Node<T> *root) const{
+		if(root == nullptr){
+			return 0;
+		}
+		if(root->data % 2 == 0){
+			return 1 + countEvens(root->left) + countEvens(root->right);
+		}
+
+		return countEvens(root->left) + countEvens(root->right);
+	}
+
+	unsigned height(Node<T>* root) const{
+		if(root == nullptr){
+			return 0;
+		}
+		unsigned heightOfLeftTree = height(root->left);
+		unsigned heightOfRightTree =  height(root->right);
+		return 1 + ((heightOfLeftTree > heightOfRightTree) ? heightOfLeftTree : heightOfRightTree);
+	}
+
+	unsigned countLeaves(Node<T> *root) const{
+		if(root == nullptr){
+			return 0;
+		}
+
+		if(root->left == nullptr && root->right == nullptr){
+			return 1;
+		}
+
+		return countLeaves(root->left) + countLeaves(root->right);
+	}
+
+	T maxLeaf(Node<T> *root) const{
+		if(root->left == nullptr && root->right == nullptr){
+			return root->data;
+		}
+
+		if(root->left != nullptr && root->right != nullptr){
+			return ((maxLeaf(root->left) > maxLeaf(root->right)) ? maxLeaf(root->left) : maxLeaf(root->right));
+		}
+
+		if(root->left != nullptr){
+			return maxLeaf(root->left);
+		}
+		T maxRightLeaf;
+		if(root->right != nullptr){
+			return maxLeaf(root->right);
+		}
+
+	}
+
+public:
+	BTree():root(nullptr){
+
+	}
+
+	bool insert(const T& data, std::string path){
+		return insert(data, path, root);
+	}
+
+	unsigned count() const{
+		return count(root);
+	}
+
+	unsigned countEvens() const{
+		return countEvens(root);
+	}
+
+	unsigned height () const{
+		return height(root);
+	}
+
+	unsigned countLeaves() const{
+		return countLeaves(root);
+	}
+
+	T maxLeaf() const{
+		return maxLeaf(root);
+	}
+	/* data */
+};
---
+++ Tree/testBTree.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include "doctest.h"
+#include "BTree.h"
+
+
+
+TEST_CASE("create tree"){
+	BTree<int> tree;
+	SUBCASE("with insert"){
+		CHECK(tree.count() == 0);
+		CHECK(tree.insert(1, "") == 1);
+		CHECK(tree.count() == 1);
+		CHECK(tree.insert(2, "L") == 1);
+		CHECK(tree.insert(3, "R") == 1);
+		CHECK(tree.insert(4, "LL") == 1);
+		CHECK(tree.insert(5, "LR") == 1);
+		CHECK(tree.insert(6, "RR") == 1);
+		CHECK(tree.insert(7, "RRR") == 1);
+
+
+		CHECK(tree.insert(1, "RRRRLLLL") == 0);
+		CHECK(tree.count() == 7);
+		CHECK(tree.countEvens() == 3);	
+	}
+}
+
+TEST_CASE("test counters"){
+	BTree<int> tree;
+	SUBCASE("count"){
+		CHECK(tree.count() == 0);
+		CHECK(tree.insert(1, "") == 1);
+		CHECK(tree.count() == 1);
+		CHECK(tree.insert(2, "L") == 1);
+		CHECK(tree.insert(3, "R") == 1);
+		CHECK(tree.insert(4, "LL") == 1);
+		CHECK(tree.insert(5, "LR") == 1);
+		CHECK(tree.insert(6, "RR") == 1);
+		CHECK(tree.insert(7, "RRR") == 1);
+
+
+		CHECK(tree.insert(1, "RRRRLLLL") == 0);
+		CHECK(tree.count() == 7);
+	}
+
+	SUBCASE("count even nodes"){
+		CHECK(tree.insert(1, "") == 1);
+		CHECK(tree.insert(2, "L") == 1);
+		CHECK(tree.insert(3, "R") == 1);
+		CHECK(tree.insert(4, "LL") == 1);
+		CHECK(tree.insert(5, "LR") == 1);
+		CHECK(tree.insert(6, "RR") == 1);
+		CHECK(tree.insert(7, "RRR") == 1);
+
+
+		CHECK(tree.insert(1, "RRRRLLLL") == 0);
+		CHECK(tree.countEvens() == 3);	
+	}
+
+	SUBCASE("height of tree"){
+		CHECK(tree.insert(1, "") == 1);
+		CHECK(tree.insert(2, "L") == 1);
+		CHECK(tree.insert(3, "R") == 1);
+		CHECK(tree.insert(4, "LL") == 1);
+		CHECK(tree.insert(5, "LR") == 1);
+		CHECK(tree.insert(6, "RR") == 1);
+		CHECK(tree.insert(7, "RRR") == 1);
+
+
+		CHECK(tree.insert(1, "RRRRLLLL") == 0);
+		CHECK(tree.height() == 4);	
+	}
+
+	SUBCASE("count tree leaves"){
+		CHECK(tree.insert(1, "") == 1);
+		CHECK(tree.insert(2, "L") == 1);
+		CHECK(tree.insert(3, "R") == 1);
+		CHECK(tree.insert(4, "LL") == 1);
+		CHECK(tree.insert(5, "LR") == 1);
+		CHECK(tree.insert(6, "RR") == 1);
+		CHECK(tree.insert(7, "RRR") == 1);
+
+
+		CHECK(tree.insert(1, "RRRRLLLL") == 0);
+		CHECK(tree.countLeaves() == 3);	
+	}
+
+	SUBCASE("max leaf"){
+		CHECK(tree.insert(1, "") == 1);
+		CHECK(tree.insert(2, "L") == 1);
+		CHECK(tree.insert(3, "R") == 1);
+		CHECK(tree.insert(4, "LL") == 1);
+		CHECK(tree.insert(5, "LR") == 1);
+		CHECK(tree.insert(6, "RR") == 1);
+		CHECK(tree.insert(7, "RRR") == 1);
+
+
+		CHECK(tree.insert(1, "RRRRLLLL") == 0);
+		CHECK(tree.maxLeaf() == 7);	
+	}
+}
