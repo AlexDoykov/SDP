4ba9b1284eba6a470f38ff6b9e97f366c6cd5832
Alexander <doykov70@gmail.com>
2017-11-14 12:04:44 +0200

    tree zzs

--- Tree/BTree.h
+++ Tree/BTree.h
@@ -2,6 +2,8 @@
 #include <string>
 #include <vector>
 #include <stack>
+#include <queue>
+#include <unordered_set>
 template<class T>
 struct Node{
 	T data;
@@ -104,9 +106,7 @@ Node<T>* root;
 	//подавайте само верни пътища защото наистина не знам какво да връщам като стойност ако го омажете
 
 	T& getElement(const char *s, Node<T>* root){
-		//std::cout<<s[0]<<" "<<root->data<<std::endl;
 		if(s[0] == '\0'){
-			//std::cout<<s[0]<<" "<<root->data<<std::endl;
 			return root->data;
 		}
 
@@ -116,10 +116,25 @@ Node<T>* root;
 		getElement(s + 1, root->right);
 	}
 
-	const Node<T>* pop(stack<Node<T>*>& st){
+	Node<T>* pop(std::stack<Node<T>*>& st){
 		Node<T>* result = st.top();
 		st.pop();
 		return result;
+	}
+
+	void checkForEqualLevels(Node<T> *root, unsigned level, std::vector<std::unordered_set<T>>& result) const{
+		if(result.size() <= level){
+			result.resize(result.size() + 1);
+		}
+		if(root != nullptr){
+			result[level].insert(root->data);
+		}
+		if(root->left != nullptr){
+			checkForEqualLevels(root->left, level + 1, result);			
+		}
+		if(root->right != nullptr){
+			checkForEqualLevels(root->right, level + 1, result);			
+		}
 	}
 
 public:
@@ -155,13 +170,14 @@ public:
 		return getElement(s, root);
 	}
 
-	std::vector<T> listLeaves () const{
+	std::vector<T> listLeaves (){
 		std::stack<Node<T>*> subTrees;
 		subTrees.push(root);
 		std::vector<T> leaves;
 		while(!subTrees.empty()){
+			//тук много ли е грешно, че така съм го написал
 			Node<T>* curr = pop(subTrees);
-			if(curr->left == nullptr && curr->right == nullptr){
+				if(curr->left == nullptr && curr->right == nullptr){
 				leaves.push_back(curr->data);
 			}
 			if(curr->right != nullptr){
@@ -173,24 +189,36 @@ public:
 		}
 		return leaves;
 	}
-
-/*	std::string findTrace (const T& x) const{
+	//not working
+	/*std::string findTrace (const T& x) const{
 		std::stack<Node<T>*> subTrees;
 		subTrees.push(root);
-		std::vector<T> leaves;
+		std::string trace;
 		while(!subTrees.empty()){
 			Node<T>* curr = subTrees.top();
 			subTrees.pop();
 			if(curr->data == x){
-				leaves.push_back(curr->data);
+				return trace;
 			}
 			if(curr->right != nullptr){
+				trace += 'R';
 				subTrees.push(curr->right);
 			}
 			if(curr->left != nullptr){
+				trace += 'L';
 				subTrees.push(curr->left);
 			}
 		}
+
+		return "_";
 	}*/
+
+	std::vector<std::unordered_set<T>> checkForEqualLevels() const{
+		std::vector<std::unordered_set<T>> result;
+		checkForEqualLevels(root, 0, result);
+		return result;
+	}
+
+
 	/* data */
 };
--- Tree/testBTree.cpp
+++ Tree/testBTree.cpp
@@ -137,4 +137,41 @@ TEST_CASE("get element of the tree"){
 		CHECK(result.size() == testResult.size());
 		CHECK(result == testResult);
 	}
+
+	/*SUBCASE("trace"){
+		CHECK(tree.insert(1, "") == 1);
+		CHECK(tree.insert(2, "L") == 1);
+		CHECK(tree.insert(3, "R") == 1);
+		CHECK(tree.insert(4, "LL") == 1);
+		CHECK(tree.insert(5, "LR") == 1);
+		CHECK(tree.insert(6, "RR") == 1);
+		CHECK(tree.insert(7, "RRR") == 1);
+
+		std::string testResult = "RRR";
+		std::string result = tree.findTrace(7);
+		CHECK(result.size() == testResult.size());
+		CHECK(result == testResult);
+	}*/
+}
+
+TEST_CASE("check equal levels"){
+	BTree<int> tree;
+	SUBCASE("check for equal levels"){
+		CHECK(tree.count() == 0);
+		CHECK(tree.insert(1, "") == 1);
+		CHECK(tree.count() == 1);
+		CHECK(tree.insert(2, "L") == 1);
+		CHECK(tree.insert(10, "R") == 1);
+		CHECK(tree.insert(4, "LL") == 1);
+		CHECK(tree.insert(5, "LR") == 1);
+		CHECK(tree.insert(6, "RR") == 1);
+		CHECK(tree.insert(7, "RRR") == 1);
+		CHECK(tree.insert(10, "LRR") == 1);
+		CHECK(tree.insert(15, "LRRL") == 1);
+
+		std::vector<std::unordered_set<int>> expectedResult = { {1}, {2, 10}, {4, 5, 6}, {7, 10}, {15}};
+		std::vector<std::unordered_set<int>> result = tree.checkForEqualLevels();
+
+		CHECK(tree.checkForEqualLevels() == expectedResult);
+	}
 }
